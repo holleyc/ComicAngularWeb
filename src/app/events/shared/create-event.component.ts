d1949d665dbcd92dc4f4d88422c47ac7b0908f17
holleyc <holleyc7@me.com>
2018-12-08 11:15:53 -0500

    Updated some of the naming in the site

--- src/app/events/shared/create-event.component.ts
+++ src/app/events/shared/create-event.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router'
     <h1>Add Comic</h1>
     <hr>
     <div class="col-md-6">
-        <h3>[Create Event Form will go here]</h3>
+        <h3>[Add new comic form will go here]</h3>
         <br/>
         <br/>
         <button type="submit" class="btn btn-primary">Save</button>
